b86b7971c950e2afe0958845f95c5df5afdf3e97
Ariel Ayala <arielayalamail@gmail.com>
2023-11-30 15:52:16 -0300

    Create product service and use it

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -14,46 +15,17 @@ import { CartService } from '../../../shared/services/cart.service';
 })
 export class ListComponent {
 
-  private cartService = inject(CartService);
-  cart = this.cartService.cart;
-  total = this.cartService.total;
   products = signal<Product[]>([]);
+  private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-
-  constructor() {
-    const initProduct: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Product 1',
-        price: 3000,
-        image: 'https://picsum.photos/640/640?r=1',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 2',
-        price: 1000,
-        image: 'https://picsum.photos/640/640?r=2',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 3',
-        price: 800,
-        image: 'https://picsum.photos/640/640?r=3',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 2',
-        price: 1500,
-        image: 'https://picsum.photos/640/640?r=4',
-        creationAt: new Date().toISOString()
-      }
-    ];
-
-    this.products.set(initProduct);
-
+  ngOnInit() {
+    this.productService.getProducts()
+      .subscribe({
+        next: (products) => {
+          this.products.set(products);
+        }
+      })
   }
 
   addToCart(product: Product) {
