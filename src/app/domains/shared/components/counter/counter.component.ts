fbdcdcfcff69bf2efccf01edfb6c6a59e4ecdf81
Ariel Ayala <arielayalamail@gmail.com>
2023-11-30 11:12:32 -0300

    Components About and Counter

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required:true}) duration = 0;
+  @Input({required:true}) message = '';
+
+
+  constructor() {
+    console.log('contructor');
+    console.log('-'.repeat(10));
+
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
+
+}
