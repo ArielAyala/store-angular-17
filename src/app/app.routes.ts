9f04909452afce11d9748bb70fa997dc8e863835
Ariel Ayala <arielayalamail@gmail.com>
2024-01-06 16:42:22 -0300

    Not found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ export const routes: Routes = [
   },
   {
     path: 'about',
-    loadComponent: () => import('./domains/info/components/about/about.component').then(m => m.AboutComponent)
+    loadComponent: () => import('./domains/info/pages/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./domains/info/pages/not-found/not-found.component').then(m => m.NotFoundComponent)
   }
 ];
