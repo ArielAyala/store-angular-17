937122d633bd5f6bb1efbba9de49f568e7d3c3ee
Ariel Ayala <arielayalamail@gmail.com>
2024-01-19 22:04:48 -0300

    Nested views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
+
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./domains/products/pages/list/list.component').then(m => m.ListComponent)
-  },
-  {
-    path: 'about',
-    loadComponent: () => import('./domains/info/pages/about/about.component').then(m => m.AboutComponent)
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./domains/products/pages/list/list.component').then(m => m.ListComponent)
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('./domains/info/pages/about/about.component').then(m => m.AboutComponent)
+      },
+    ]
   },
   {
     path: '**',
