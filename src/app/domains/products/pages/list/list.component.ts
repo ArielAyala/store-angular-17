d8a462b34e0a5e4e0d68718166854d08a110a0e0
Ariel Ayala <arielayalamail@gmail.com>
2024-01-22 13:32:07 -0300

    Get categories and show them on the left side of the layout

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 
 @Component({
@@ -17,20 +19,42 @@ import { ProductService } from '@shared/services/product.service';
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
-    this.productService.getProducts()
-      .subscribe({
-        next: (products) => {
-          this.products.set(products);
-        }
-      })
+    this.getProducts();
+    this.getCategories();
   }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
+    this.productService.getProducts()
+      .subscribe({
+        next: (products) => {
+          this.products.set(products);
+        },
+        error: () => {
+
+        },
+      })
+  }
+
+  private getCategories() {
+    this.categoryService.getCategories()
+      .subscribe({
+        next: (categories) => {
+          this.categories.set(categories);
+        },
+        error: () => {
+
+        },
+      })
   }
 
 }
