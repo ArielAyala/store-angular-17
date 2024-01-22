19280c14b0c11f17b55cad23cf2148f604157737
Ariel Ayala <arielayalamail@gmail.com>
2024-01-22 13:49:37 -0300

    Add category filter using input and listen the query params

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
 import { ProductComponent } from '@product/components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Product } from '@shared/models/product.model';
@@ -7,12 +7,13 @@ import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
 import { Category } from '@shared/models/category.model';
+import { RouterLinkWithHref } from '@angular/router';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -23,10 +24,20 @@ export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit() {
     this.getProducts();
     this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // const category_id = changes['category_id'];
+    // if (category_id) {
+    //   this.getProducts();
+    // }
+
+    this.getProducts();
   }
 
   addToCart(product: Product) {
@@ -34,7 +45,7 @@ export class ListComponent {
   }
 
   private getProducts() {
-    this.productService.getProducts()
+    this.productService.getProducts(this.category_id)
       .subscribe({
         next: (products) => {
           this.products.set(products);
